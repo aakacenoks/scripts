a408d4fd8d53aef9fbe0c05dd5ec06b29e32de13
tdl.andrisa <andris.akacenoks@testdevlab.com>
2021-04-29 12:15:51 +0300

    Write to file

--- prettify_xml.rb
+++ prettify_xml.rb
@@ -1,11 +1,15 @@
 require 'nokogiri'
 
-def pretty_print_xml(xml='')
+def prettify_xml(xml='')
   doc = Nokogiri.XML(xml) do |config|
     config.default_xml.noblanks
   end
-  puts doc.to_xml(:indent => 2)
+  doc.to_xml(:indent => 2)
 end
 
 xml = File.read(ARGV[0])
-puts pretty_print_xml(xml)
+pretty_xml = prettify_xml(xml)
+
+File.open(ARGV[1], 'w') { |file| file.write(pretty_xml) }
+
+
